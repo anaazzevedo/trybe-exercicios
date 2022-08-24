b60284dfad61811b713badac22096cc10f0974fb
Ana <anateixeirazevedo@gmail.com>
2022-08-23 22:48:19 -0300

    Adiciona exemplos do conteúdo

---
+++ fundamentos/bloco-4-intro-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exemplo.js
@@ -0,0 +1,12 @@
+//1 - Crie um objeto player contendo as variáveis listadas abaixo.
+
+let player = {
+    nome: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3
+    }
+}
+console.log(player['nome']);
