e9813cbf3d3596de4eecfe4b00295ba1143ffe32
Ana <anateixeirazevedo@gmail.com>
2022-11-03 10:48:52 -0300

    Adiciona exercícios do dia 1.2

---
+++ frontEnd/secao-1-javascript-assincrono-e-callbacks/dia-2-fetch-API-e-async/tryCatch.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+// usando try e catch
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch(error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
+}
+
+fetchJoke();
